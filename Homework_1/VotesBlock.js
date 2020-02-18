64edf7e00ced570d6b96bc67d11ded6081738c0f
Igor <panama1790@gmail.com>
2020-02-18 23:06:37 +0300

    Add homework

---
+++ Homework_1/VotesBlock.js
@@ -0,0 +1,37 @@
+﻿var VotesBlock = React.createClass({
+
+    displayName: 'VotesBlock',
+
+    getDefaultProps: function () {
+        return {shop: "Не удалось получить название магазина"}
+    },
+
+    render: function () {
+
+        let goodsCode = [];
+        this.props.goods.forEach(elem => {
+            let goodCode =
+                React.DOM.tr({key: elem.code, className: 'Good'},
+                    React.DOM.td(null, elem.title),
+                    React.DOM.td(null, elem.price + ' USD'),
+                    React.DOM.td(null, elem.quantity),
+                    React.DOM.td(null, React.DOM.img({src: elem.imgURL})),
+                );
+            goodsCode.push(goodCode)
+        });
+
+        return React.DOM.div({className: 'VotesBlock'},
+            React.DOM.div({className: 'Shop'}, 'Магазин ' + this.props.shop),
+            React.DOM.table({className: 'table'},
+                React.DOM.tr(null,
+                    React.DOM.th(null, 'Название'),
+                    React.DOM.th(null, 'Цена'),
+                    React.DOM.th(null, 'Остаток'),
+                    React.DOM.th(null, 'Изображение'),
+                ),
+                ({className: 'Goods'}, goodsCode)
+            ),
+        );
+    },
+});
+
